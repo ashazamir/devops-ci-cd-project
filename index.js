b13f17ada8bcb410739c4d100da50c6252b60484
ashazamir <ashazamir03@gmail.com>
2026-05-22 09:25:15 +0800

    Add CI/CD pipeline

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'DevOps CI/CD Pipeline Working!',
+    status: 'success'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
